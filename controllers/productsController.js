51d06d499dcd5c19209eecaf878ad94c31b319fe
JessiLopezObando <jessicaandrealopezobando@gmail.com>
2022-11-07 12:52:02 -0500

    Modelo - Ver lista de productos

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,6 +1,10 @@
+const producto=require("../models/productsModel")
+
+//Ver la lista de productos
 exports.getProducts=(req,res,next) => {
     res.status(200).json({
        success:true,
        message: "En esta ruta usted va a poder ver todos los productos"
     })
 }
+
---
+++ models/productsModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const productsScheme = mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Por favor registra el nombre del producto."],
+        trim:true,
+        maxLength:[120,"El nombre del producto no debe exceder los 120 caracteres."]
+    },
+    price:{
+        type: Number,
+        required:[true,"Por favor registre el precio del producto."],
+        maxLength:[8, "El precio del producto no puede estar por encima de 99'999.999"],
+        default: 0.0 //reconoce que es un float
+    },
+    description:{
+        type:String,
+        required:[true,"Por favor registre una descripcion para el producto."]
+    },
+    stock:{
+        type: Number,
+        required:[true,"Por favor registre el precio del producto."],
+        default: 0
+    },
+    imagen:[
+        {
+            public_id:{
+                type:String,
+                required:true
+            },
+            url:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+})
+
+module.exports = mongoose.model('products', productsScheme)
