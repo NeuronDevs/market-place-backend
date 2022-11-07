156db47a907ae4a2ab090f915bf9cc3148cb5766
Luis <luissolivaresp@gmail.com>
2022-11-07 16:50:14 -0500

    productsModel

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,36 +1,36 @@
 const mongoose = require("mongoose");
 
 const productsScheme = mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,"Por favor registra el nombre del producto."],
-        trim:true,
-        maxLength:[120,"El nombre del producto no debe exceder los 120 caracteres."]
+    name: {
+        type: String,
+        required: [true, "Por favor registra el nombre del producto."],
+        trim: true,
+        maxLength: [120, "El nombre del producto no debe exceder los 120 caracteres."]
     },
-    price:{
+    price: {
         type: Number,
-        required:[true,"Por favor registre el precio del producto."],
-        maxLength:[8, "El precio del producto no puede estar por encima de 99'999.999"],
+        required: [true, "Por favor registre el precio del producto."],
+        maxLength: [8, "El precio del producto no puede estar por encima de 99'999.999"],
         default: 0.0 //reconoce que es un float
     },
-    description:{
-        type:String,
-        required:[true,"Por favor registre una descripcion para el producto."]
+    description: {
+        type: String,
+        required: [true, "Por favor registre una descripcion para el producto."]
     },
-    stock:{
+    stock: {
         type: Number,
-        required:[true,"Por favor registre el precio del producto."],
+        required: [true, "Por favor registre el precio del producto."],
         default: 0
     },
-    imagen:[
+    images: [
         {
-            public_id:{
-                type:String,
-                required:true
+            public_id: {
+                type: String,
+                required: true
             },
-            url:{
-                type:String,
-                required:true
+            url: {
+                type: String,
+                required: true
             }
         }
     ],
