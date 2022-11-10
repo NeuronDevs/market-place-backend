88474c4e54f53a4d3dffe3255aa47202c984a059
JessiLopezObando <jessicaandrealopezobando@gmail.com>
2022-11-10 12:40:02 -0500

    Fixing errors in merge app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app=express();
 const errorMiddleware= require("./middlewares/errors")
 const express = require("express");
 const cookieParser = require('cookie-parser');
-const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
