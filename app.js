78b3d14408f3f473fb2dd85d1ce2bb3c723bd0da
JessiLopezObando <jessicaandrealopezobando@gmail.com>
2022-11-11 15:24:14 -0500

    Order (routes(admin and user), controllers (admin and user)

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express=require("express");
-const app=express();
+const app = express();
 const errorMiddleware= require("./middlewares/errors")
-const express = require("express");
 const cookieParser = require('cookie-parser');
 
 app.use(express.json());
 app.use(cookieParser());
+
 //Importar rutas
 const productos = require("./routes/products");
 const usuarios = require("./routes/auth");
+const ordenes=require("./routes/orders")
 
 app.use('/api', usuarios)
 app.use('/api', productos) // sujeto a decision (ruta del navegador)
+app.use('/api', ordenes)
 
 //MiddleWare para manejar errores
 app.use(errorMiddleware)
