de3d76058b1a31771e3d20d36afcf2cd7939f935
Luis <luissolivaresp@gmail.com>
2022-11-13 16:31:28 -0500

    update products

--- routes/products.js
+++ routes/products.js
@@ -10,7 +10,7 @@ const { getProducts,
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/authMiddleware");
 
 router.route('/productos').get(getProducts) // Establecemos desde que ruta queremos ver el getProducts
-router.route('/productos/nuevo').post(isAuthenticatedUser, authorizeRoles("admin"), newProduct) // Establecemos desde que ruta queremos ver el newProduct
+router.route('/producto/nuevo').post(isAuthenticatedUser, authorizeRoles("admin"), newProduct) // Establecemos desde que ruta queremos ver el newProduct
 router.route('/producto/:id').get(getProductById) // Establecemos desde que ruta queremos ver el getProductById
 router.route('/producto/:id').put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct) // Creación de la ruta de actualización
 router.route('/producto/:id').delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct) // Creación de la ruta de eliminación por Id
